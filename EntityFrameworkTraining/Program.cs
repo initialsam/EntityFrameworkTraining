96341aebf01fdb3ee3746cd5f44705e9b72517aa
sam <evaoosyz@gmail.com>
2018-08-25 22:52:22 +0800

    完成 UpdateDate 方法

--- EntityFrameworkTraining/Program.cs
+++ EntityFrameworkTraining/Program.cs
@@ -11,12 +11,13 @@ namespace EntityFrameworkTraining
     {
         static void Main(string[] args)
         {
-            AddData();
+            UpdateData();
             Console.ReadLine();
         }
         static void AddData()
         {
             DataContext context = new DataContext();
+            context.Database.Log = (log) => Console.WriteLine(log);
             Customer c1 = new Customer() { Name = "AAA" };
             context.Customer.Add(c1);
             Customer c2 = new Customer() { Name = "BBB" };
@@ -25,5 +26,15 @@ namespace EntityFrameworkTraining
             context.Customer.Add(c3);
             context.SaveChanges();
         }
+        static void UpdateData()
+        {
+            DataContext context = new DataContext();
+            context.Database.Log = (log) => Console.WriteLine(log);
+
+            var item = context.Customer.FirstOrDefault(a => a.ID == 3);
+            item.Name = "ZZZ";
+            context.SaveChanges();
+        }
+      
     }
 }
